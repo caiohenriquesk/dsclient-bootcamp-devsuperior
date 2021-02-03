92d687f6cd446bce4fb01fba5c69100eacd1759e
Caio Henrique <caiohenriquesk@gmail.com>
2021-02-03 13:59:29 -0300

    Feature: Seeds SQL

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Caio', '86389120052', 1350.0, '1995-08-23T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe', '04998114026', 3500.0, '1990-04-05T00:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernando', '00135731011', 2150.0, '1987-10-20T00:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carol', '45050766010', 2000.0, '1991-03-25T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Denise', '40130481084', 359.0, '2005-01-12T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('José', '12871548080', 4000.0, '1979-04-27T00:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana', '74101570035', 1570.0, '1990-05-11T00:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Karina', '98666361000', 3790.0, '1992-11-14T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Thiago', '68916064031', 1100.0, '2001-02-17T00:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Derick', '85841943049', 2350.0, '1988-07-19T00:00:00Z', 1);
