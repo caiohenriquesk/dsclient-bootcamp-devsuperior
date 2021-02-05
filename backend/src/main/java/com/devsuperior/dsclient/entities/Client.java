e1bc94eddc8456946c121fbaf83071c858528e85
Caio Henrique <caiohenriquesk@gmail.com>
2021-02-05 00:19:02 -0300

    Feature: DELETE Client + DatabaseException

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -10,8 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.devsuperior.dsclient.dto.ClientDTO;
-
 @Entity
 @Table(name = "tb_client")
 public class Client implements Serializable {
@@ -37,15 +35,6 @@ public class Client implements Serializable {
 		this.income = income;
 		this.birthDate = birthDate;
 		this.children = children;
-	}
-	
-	public Client(ClientDTO entity) {
-		this.id = entity.getId();
-		this.name = entity.getName();
-		this.cpf = entity.getCpf();
-		this.income = entity.getIncome();
-		this.birthDate = entity.getBirthDate();
-		this.children = entity.getChildren();
 	}
 
 	public Long getId() {
--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
 import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
@@ -36,7 +39,8 @@ public class ClientService {
 
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
-		Client entity = new Client(dto);
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		
 		return new ClientDTO(entity);
@@ -46,16 +50,30 @@ public class ClientService {
 	public ClientDTO update(Long id, ClientDTO dto) {
 		try {
 			Client entity = repository.getOne(id);
-			entity.setName(dto.getName());
-			entity.setBirthDate(dto.getBirthDate());
-			entity.setChildren(dto.getChildren());
-			entity.setCpf(dto.getCpf());
-			entity.setIncome(dto.getIncome());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 	
 }
